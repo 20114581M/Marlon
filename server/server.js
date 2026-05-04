c911fb97ff1f3e1fbabbe9cd335187af47f88865
unknown <20114581@s.ubaguio.edu>
2026-05-03 23:01:27 -0400

    fixingeverylinkserver2

--- server/server.js
+++ server/server.js
@@ -58,12 +58,12 @@ app.delete("/contact/:id", async (req, res) => {
     }
 });
 
-// Serve static frontend files
-app.use(express.static(path.join(__dirname, "../")));
+// Serve static frontend files from dist
+app.use(express.static(path.join(__dirname, "../dist")));
 
 // Catch-all route for frontend (Express 5 compatible)
 app.get("/{*path}", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
